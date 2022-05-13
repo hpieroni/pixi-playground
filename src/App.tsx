0a12b28af6046d7dcc0c896b1c642082c7943666
Hernan Pieroni <hernan.pieroni@autocloud.dev>
2022-05-12 21:13:27 -0300

    feat: initial app layout with material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Drawer,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  List,
+  Typography,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const drawerWidth = 240;
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Toolbar>
+            <Typography variant="h6" component="h1">
+              Pixi.js Playground
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Toolbar />
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Simple Example" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Typography paragraph>Hello world</Typography>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
