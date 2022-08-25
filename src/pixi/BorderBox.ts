25c8e5156bf227e3c792b0868f4180a35f876bdc
Hernan Pieroni <hernan.pieroni@autocloud.dev>
2022-08-25 11:31:23 -0300

    feat: allow a border box to only have one target

--- src/pixi/BorderBox.ts
+++ src/pixi/BorderBox.ts
@@ -1,9 +1,11 @@
 import { Graphics } from "pixi.js";
 
+type Border = "top" | "right" | "bottom" | "left";
 export interface BorderConfig {
   width: number;
   color?: number;
   alpha?: number;
+  only?: Border;
 }
 
 class BorderBox extends Graphics {
@@ -15,7 +17,27 @@ class BorderBox extends Graphics {
 
     const offset = config.width / 2;
     this.lineStyle(config.width, config.color, config.alpha);
-    this.drawRect(offset, offset, dimension.width, dimension.height);
+
+    switch (config.only) {
+      case "top":
+        this.moveTo(config.width, offset);
+        this.lineTo(dimension.width, offset);
+        break;
+      case "right":
+        this.moveTo(dimension.width + offset, config.width);
+        this.lineTo(dimension.width + offset, dimension.height);
+        break;
+      case "bottom":
+        this.moveTo(config.width, dimension.height + offset);
+        this.lineTo(dimension.width, dimension.height + offset);
+        break;
+      case "left":
+        this.moveTo(offset, config.width);
+        this.lineTo(offset, dimension.height);
+        break;
+      default:
+        this.drawRect(offset, offset, dimension.width, dimension.height);
+    }
   }
 }
 
